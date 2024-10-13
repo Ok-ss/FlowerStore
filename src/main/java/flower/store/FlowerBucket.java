a3f39821d226ef4a3c3a42e64dd474c20999c385
Ok-ss <kotliarchuk.pn@ucu.edu.ua>
2024-10-13 21:42:31 +0300

    Ultimate commit

--- src/main/java/flower/store/FlowerBucket.java
+++ src/main/java/flower/store/FlowerBucket.java
@@ -1,51 +1,34 @@
 package flower.store;
 import java.util.ArrayList;
-import java.util.Optional;
 
 
 public class FlowerBucket {
     ArrayList<FlowerPack> packs;
+    ArrayList<FlowerType> types;
+    private double price;
 
     FlowerBucket(){
         this.packs = new ArrayList<>();
+        this.types = new ArrayList<>();
     }
 
     FlowerBucket(ArrayList<FlowerPack> packs) {
         this.packs = packs;
+        for (FlowerPack p:packs){
+            this.types.add(p.getFlower().getFlowerType());
+        }
     }
 
     public double getPrice(){
-        double price = 0;
-        for (int i = 0; i<packs.size(); i++){
-            price += packs.get(i).getPrice();
-        }
         return price;
     }
 
     public void add(FlowerPack pack) {
         this.packs.add(pack);
+        this.price += pack.getPrice();
+        if (!types.contains(pack.getFlower().getFlowerType()))
+            this.types.add(pack.getFlower().getFlowerType());
     }
 
-    public ArrayList<FlowerPack> search(Optional<FlowerType> flower, 
-                                        Optional<Integer> quantity, 
-                                        Optional<Double> price) {
-        ArrayList<FlowerPack> res = new ArrayList<>();
-        for (int i = 0; i<packs.size(); i++) {
-            boolean matches = true;
-            FlowerPack pack = packs.get(i);
-            if (flower.isPresent() && !flower.equals(Optional.of(pack.getFlower().getFlowerType()))){
-                matches = false;
-            }
-            if (quantity.isPresent() && !quantity.equals(Optional.of(pack.getQuantity()))){
-                matches = false;
-            }
-            if (price.isPresent() && !price.equals(Optional.of(pack.getPrice()))){
-                matches = false;
-            }
-            if (matches) {
-                res.add(pack);
-            }
-        }
-        return res;
-    }
+    
 }
